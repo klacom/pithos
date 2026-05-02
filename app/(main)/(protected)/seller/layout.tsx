b0a372c13a12244235d91601f65db5f3fe10b736
Lark Bolotaolo <artysl4yer@gmail.com>
2026-05-02 09:31:12 +0800

    Fix warning chidrens to a much better layout

--- app/(main)/(protected)/seller/layout.tsx
+++ app/(main)/(protected)/seller/layout.tsx
@@ -1,14 +1,15 @@
 import { ReactNode } from 'react'
 import SuspendedNav from '@/components/SuspendedNav'
 import SellerMainLayout from '@/components/SellerMainLayout'
+type Props = {children: ReactNode}
 
-const layout = ({children} : {children : ReactNode}) => {
+export default function Layout({ children }: Props) {
   return (
-    <div className='h-full w-full flex flex-col items-center'>
-        <SuspendedNav/>
-        <SellerMainLayout children={children} />
+    <div className="h-full w-full flex flex-col items-center">
+      <SuspendedNav />
+      <SellerMainLayout>
+        {children}
+      </SellerMainLayout>
     </div>
   )
 }
-
-export default layout
